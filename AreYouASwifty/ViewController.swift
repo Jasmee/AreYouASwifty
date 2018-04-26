2ddd5e69541e0c5eff90eb552c8ced071a0ef3fc
Jasmee Sengupta <jasmee.sengupta@in.ibm.com>
2018-04-26 14:15:19 +0530

    Initial code.

--- AreYouASwifty/ViewController.swift
+++ AreYouASwifty/ViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//var name3: String class vc has no initializers
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print(name3)
+        
+        var name: String
+        //print(name)// used b4 being initialed
+        
+        var name1: String?
+        print(name1) // nil
+        //print(name1!) // fatal error: unexpectedly found nil while unwrapping an optional value
+        
+        var name2: String!
+        print(name2)// this also prints nil, how?
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
